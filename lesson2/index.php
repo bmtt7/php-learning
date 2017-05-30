159c0ef6a1105eab7526153c7e16b5b83f90dd17
Mauro de Carvalho <mauro.oak@outlook.com>
2017-05-30 01:40:03 -0300

    Lesson 2

---
+++ lesson2/index.php
@@ -0,0 +1,4 @@
+<?php
+	$greeting = "Hello, world";
+	require 'index.view.php';
+?>
---
+++ lesson2/index.view.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Document</title>
+	<style type="text/css">
+		header{
+			background: #e3e3e3;
+			padding: 2em;
+			text-align: center;
+		}
+	</style>
+</head>
+<body>
+	<header>
+		<h1>
+			<?=
+				$greeting;	
+			?>
+		</h1>
+	</header>
+</body>
+</html>
