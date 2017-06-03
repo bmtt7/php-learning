14891c722e76c3d08b3d2d32eb41ac1b6211fb90
Mauro de Carvalho <mauro.oak@outlook.com>
2017-06-02 23:24:53 -0300

    criando config.php para segurança

--- lesson9/bootstrap.php
+++ lesson9/bootstrap.php
@@ -1,8 +1,9 @@
 <?php
 	require 'database/Connection.php';
 	require 'database/QueryBuilder.php';
+	$config = require 'database/config.php';
 
 	// $pdo = Connection::make();
-	return new QueryBuilder(Connection::make());
+	return new QueryBuilder(Connection::make($config['database']));
 
 ?>
---
+++ lesson9/database/config.php
@@ -0,0 +1,11 @@
+<?php
+return [
+	'database' => [
+		'name' => 'mytodo', 
+		'username' => 'root',
+		'password' => '123456',
+		'connection' => 'mysql:host=127.0.0.1',
+		'options' => []
+	]
+];
+?>
